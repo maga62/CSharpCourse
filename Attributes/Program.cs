1d958b76a4a4c9111bf59fc3552535f379775599
maga62 <muhammad709059952@gmail.com>
2021-06-01 03:23:12 +0300

    neww

---
+++ Attributes/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Attributes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Customer customer = new Customer { Id = 1, LastName = "Muho", Age = 22 };
+            CustomerDal customerDal = new CustomerDal();
+            customerDal.Add(customer);
+            Console.ReadLine();
+        }
+    }
+    [ToTable("Customers")]
+    class Customer
+    {
+        public int Id { get; set; }
+        // firstname girilmesi zorunlu olsun 
+        [RequiredProperty]
+        public string FirstName { get; set; }
+        [RequiredProperty]
+        public string LastName { get; set; }
+        [RequiredProperty]
+        public int Age { get; set; }
+    }
+    class CustomerDal
+    {
+        public void Add(Customer customer)
+        {
+            Console.WriteLine("{0},{1},{2},{3} adddeddd! ", customer.Id, customer.FirstName, customer.LastName, customer.Age);
+        }
+    }
+    [AttributeUsage(AttributeTargets.Property | AttributeTargets.Method)]
+    class RequiredPropertyAttribute:Attribute
+    {
+
+    }
+    class ToTableAttribute : Attribute
+    {
+        private string _tableName;
+
+        public ToTableAttribute(string tableName)
+        {
+            _tableName = tableName;
+        }
+    }
+
+}
